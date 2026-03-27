29b8e7368a8cbbf553e37b708402753c8576f734
kuldeepumaraiya <kuldeepumaraiya@gmail.com>
2026-03-27 16:21:07 +0530

    about section update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,6 @@
 import Hero from '../components/Hero';
 import Ticker from '../components/Ticker';
-import About from '../components/About';
 import Tools from '../components/Tools';
-import Experience from '../components/Experience';
-import Education from '../components/Education';
 import Projects from '../components/Projects';
 import Resume from '../components/Resume';
 
@@ -12,10 +9,7 @@ const Home = () => {
     <>
       <Hero />
       <Ticker />
-      <About />
       <Tools />
-      <Experience />
-      <Education />
       <Projects />
       {/* <Resume /> */}
     </>
